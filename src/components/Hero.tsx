3898cd640b17747a92187a12e376899df680e911
Rey Matugas <reyjoelmatugas@gmail.com>
2026-03-18 22:40:34 +0800

    Modified the Hero section and add the About section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -83,7 +83,7 @@ const Hero = () => {
             
             <div className='view-cocktails'>
               <p className='subtitle'>
-                Every cocktail on our menu is a blend of premium ingredients, creative, flair, and timeless recipes - designed to delight your senses.
+                Every cocktail on our menu is a blend of premium ingredients, creative, flair, and timeless recipes — designed to delight your senses.
               </p>
               <a href='#cocktails'>View Cocktails</a>
             </div>
